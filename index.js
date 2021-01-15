a4808cf61dce9f0482a038870c10979be121b1ca
chance9077 <675409431@qq.com>
2021-01-15 18:32:21 +0800

    init

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const http = require('http')
+const internalIp = require('internal-ip')
+const publicIp = require('public-ip')
+
+const log = str => process.stdout.write(str + '\n')
+
+internalIp.v4().then(val => log(val))
+publicIp.v4().then(val => log(val))
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
@@ -6,4 +13,4 @@ const server = http.createServer((req, res) => {
   res.end('hello :', req.url)
 })
 
-server.listen(8080, '0.0.0.0', () => process.stdout.end('server is running.\n'))
+server.listen(8080, '0.0.0.0', () => log('server is running'))
