a4f9fc768d28caf4c5ba256fe6134de43cbbad7b
chance9077 <675409431@qq.com>
2021-01-15 17:29:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('hello :', req.url)
+})
+
+server.listen(8080, '0.0.0.0', () => console.log('Server running.'))
